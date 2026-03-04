9ed311f73e24d479181fb4413bdc9d29d2a37457
Junaid <junaidadams117@gmail.com>
2026-03-04 13:23:46 +0200

    Fixed vite setup etc

---
+++ src/components/Wrapper.jsx
@@ -0,0 +1,7 @@
+import React, { Children } from "react";
+
+const Wrapper = () => {
+  return <div className="bg-white"></div>;
+};
+
+export default Wrapper;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div className="bg-black w-full min-h-screen">s</div>;
+};
+
+export default Home;
